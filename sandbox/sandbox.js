c290c32d11399248765d4c9c31b389dd50d68cf6
Felix Niklas <mrflix@gmail.com>
2012-10-13 11:38:51 +0200

    add ctrl+enter support

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -141,7 +141,7 @@ function loadFile() {
 window.addEventListener("hashchange", loadFile, false);
 window.addEventListener("keydown", function(event){
   // ctrl and enter = render
-  if (event.ctrlKey && event.keyCode === 13) {
+  if((event.ctrlKey || event.metaKey) && event.which === 13) {
     event.preventDefault();
     render();
   }
